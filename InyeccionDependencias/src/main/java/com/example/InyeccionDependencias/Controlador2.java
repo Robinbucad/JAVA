aaa472c58d25341399608fc0039279968e85219b
BOSONIT\robin.bucad <robin.bucad6@gmail.com>
2022-07-13 23:26:56 +0200

    controlador 2

--- InyeccionDependencias/src/main/java/com/example/InyeccionDependencias/Controlador2.java
+++ InyeccionDependencias/src/main/java/com/example/InyeccionDependencias/Controlador2.java
@@ -1,4 +1,25 @@
 package com.example.InyeccionDependencias;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controlador2 {
+
+    @Autowired
+    PersonaInt personaInt;
+    @GetMapping("/pong")
+    public String getPing(){
+        return "ping";
+    }
+    @GetMapping("/controlador2/getPersona")
+    PersonaInt getEdad2(){
+        System.out.println(Integer.parseInt(personaInt.getEdad()) * 2);
+        return personaInt;
+    }
+
+
+
 }
