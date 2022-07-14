cf6a1fb2791f36a541e1d3480677bb64d328a173
BOSONIT\robin.bucad <robin.bucad6@gmail.com>
2022-07-14 15:34:21 +0200

    BS3 start

---
+++ BS3/src/main/java/com/example/BS3/Bs3Application.java
@@ -0,0 +1,13 @@
+package com.example.BS3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs3Application.class, args);
+	}
+
+}
