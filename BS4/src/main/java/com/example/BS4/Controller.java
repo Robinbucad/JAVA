d61b490d3c20d6fc0b611675aafedfe8170d0528
BOSONIT\robin.bucad <robin.bucad6@gmail.com>
2022-07-15 18:06:53 +0200

    bs4 finished

---
+++ BS4/src/main/java/com/example/BS4/Controller.java
@@ -0,0 +1,29 @@
+package com.example.BS4;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @Value("${key.VAR1}")
+    private String var1;
+
+    @Value("${key.VAR2}")
+    private String var2;
+
+    @Value("${key.VAR3: No tiene valor}")
+    private String var3;
+
+    @GetMapping("/valores")
+    public String getValue(){
+        return "Valor de var1 es: " + var1 + " valor de my.var es: " + var2;
+    }
+
+    @GetMapping("/var3")
+    public String getValue3(){
+        return "El valor de var3 es: " + var3;
+    }
+
+}
