5531ce45e5949729b35670bd93bb0d4fedf34ab9
BOSONIT\robin.bucad <robin.bucad6@gmail.com>
2022-07-19 13:46:25 +0200

    relacion entre tablas init

---
+++ relacionEntreTablas/src/main/java/com/example/relacionEntreTablas/RelacionEntreTablasApplication.java
@@ -0,0 +1,13 @@
+package com.example.relacionEntreTablas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionEntreTablasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionEntreTablasApplication.class, args);
+	}
+
+}
---
+++ relacionEntreTablas/src/test/java/com/example/relacionEntreTablas/RelacionEntreTablasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relacionEntreTablas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionEntreTablasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
